7ee0b219a62945f3dae344ec41a902fe588ecce5
anA <129904087+analeguizamon@users.noreply.github.com>
2023-10-20 19:41:55 -0300

    Delete etc directory

--- etc/conn.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-#$conn = new mysqli("localhost", "root", "", "id21426390_ticketease");
-$conn = new mysqli("localhost", "id21426390_ticketease", "Ticketease#123", "id21426390_ticketease");
-
-if ($conn->connect_error) die("Error de conexión: " . $conn->connect_error);
-
-?>
